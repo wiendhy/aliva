484ce887df9e4fdfb326e44279b86808b3fd03ec
wiendhy <43066026+wiendhy@users.noreply.github.com>
2020-08-05 00:18:45 +0800

    Add files via upload

---
+++ java-one.js
@@ -0,0 +1,148 @@
+eval(function (_0x5c3dx1, _0x5c3dx2, _0x5c3dx3, _0x5c3dx4, _0x5c3dx5, _0x5c3dx6) {
+    _0x5c3dx5 = function (_0x5c3dx3) {
+        return (_0x5c3dx3 < _0x5c3dx2 ? "" : _0x5c3dx5(parseInt(_0x5c3dx3 / _0x5c3dx2))) + ((_0x5c3dx3 = _0x5c3dx3 % _0x5c3dx2) > 35 ? String.fromCharCode(_0x5c3dx3 + 29) : _0x5c3dx3.toString(36))
+    };
+    if (!"".replace(/^/, String)) {
+        while (_0x5c3dx3--) {
+            _0x5c3dx6[_0x5c3dx5(_0x5c3dx3)] = _0x5c3dx4[_0x5c3dx3] || _0x5c3dx5(_0x5c3dx3)
+        };
+        _0x5c3dx4 = [function (_0x5c3dx5) {
+            return _0x5c3dx6[_0x5c3dx5]
+        }];
+        _0x5c3dx5 = function () {
+            return "\\w\+"
+        };
+        _0x5c3dx3 = 1
+    };
+    while (_0x5c3dx3--) {
+        if (_0x5c3dx4[_0x5c3dx3]) {
+            _0x5c3dx1 = _0x5c3dx1.replace(new RegExp("\\b" + _0x5c3dx5(_0x5c3dx3) + "\\b", "g"), _0x5c3dx4[_0x5c3dx3])
+        }
+    };
+    return _0x5c3dx1
+}('6 1r\(\)\{7 e=2m 2n,o=e.2o\(\),n=\(e.2p\(\),e.2q\(\)\),t="U";12>n&&\(11.14\("15 16 17 "\+n\+":"\+o\+" U"\),n=n,t="U"\),12==n&&\(11.14\("15 16 17 "\+n\+":"\+o\+" 18"\),t="18"\),24==n&&\(11.14\("15 16 17 "\+\(n-12\)\+":"\+o\+" U"\),n=12,t="U"\),n>12&&\(11.14\("15 16 17 "\+\(n-12\)\+":"\+o\+" 18"\),n-=12,t="18"\),10>o&&\(o="0"\+o\),10>n&&\(n="0"\+n\),A.1D\("2r"\).1E=n,A.1D\("2s"\).1E=o\+":"\+t\}1r\(\),2t\(1r,2u\);\$\(".P-19-1a"\).D\("E",6\(\)\{\$\(".P-19"\).q\("P-1a"\)\}\),\$\(".P-19-1F"\).D\("E",6\(\)\{\$\(".P-19"\).p\("P-1a"\)\}\);7 1s="1G://2v.2w.2x/v/2y.2z";7 2A=\[,"2B","2C","2D","2E","2F","2G","2H","2I","2J","2K","2L","2M"\];\$\(6\(\)\{7 e=\$\(A\).1b\(\),o=\$\(".F-G"\).2N\(\);\$\(1t\).V\(6\(\)\{7 s=\$\(A\).1b\(\);\$\(A\).1b\(\)>=2O\?\$\(".F-G"\).1H\("1I","1J"\):\$\(".F-G"\).1H\("1I","1J"\),s>o\?\$\(".F-G"\).q\("V"\):\$\(".F-G"\).p\("V"\),s>e\?\$\(".F-G"\).p\("w-V"\):\$\(".F-G"\).q\("w-V"\),e=\$\(A\).1b\(\)\}\)\}\);\$\(".H-1K .1L .1M-I"\).B\(6\(\)\{7 e=\$\(4\).k\("W"\).g\("1u-x"\),t=\(\$\(4\).k\("W"\).g\("1u-w"\),\$\(4\).1v\(\).g\("1w"\)\),a=\$\(4\).k\("W"\).g\("1u-1x"\);a.2P\("H"\)&&\$.1N\(\{1c:"/1O/1d/J/-/"\+e\+"\?1e=1P-1Q-1R&1S-1T=3",1x:"1U",1V:"1W",1X:6\(e\)\{1f\(7 a="",r="<1Y>",n=0;n<e.8.9.1g;n\+\+\)\{1f\(7 i=0;i<e.8.9\[n\].Q.1g;i\+\+\)C\("1Z"==e.8.9\[n\].Q\[i\].20\)\{a=e.8.9\[n\].Q\[i\].z;21\}7 l=e.8.9\[n\].j.\$t,s=e.8.9\[n\].22\[0\].23,d=e.8.9\[n\].I.\$t,c=\$\("<b>"\).1h\(d\);C\(d.1i\("//25.26.1y/27/"\)>-1\)7 f=e.8.9\[n\].28\$29.1c,o=f;1j C\(d.1i\("<v"\)>-1\)7 h=c.k\("v:2a"\).g\("1k"\),o=h;1j 7 o=1s;r\+=\'<1l><b m="2b"><a z="\'\+a\+\'"><v 1e="\'\+l\+\'" 1k="\'\+o\+\'" j="\'\+l\+\'"/></a></b><b m="H-2Q"><a m="2R \'\+s\+\'" z="/2S/x/\'\+s\+\'" j="\'\+s\+\'"></a></b><b m="H-I"><1m m="H-j"><a z="\'\+a\+\'" j="\'\+l\+\'">\'\+l\+\'</a></1m></b><b m="2c"/></1l>\'\}r\+="</1Y>",\$\(".H-1K .1L .1M-I"\).B\(6\(\)\{7 e=\$\(4\).1v\(\).g\("1w"\);e==t&&\(\$\(4\).1h\(r\),\$\(4\).1v\(\).q\("H"\),\$\(4\).k\(".2b"\).B\(6\(\)\{\$\(4\).g\("1n",6\(e,t\)\{K t.R\("/J.S","/1z.S"\)\}\).g\("1n",6\(e,t\)\{K t.R\("1A-c","1B"\)\}\)\}\)\)\}\)\}\}\)\}\),\$\("#L-1d"\).B\(6\(\)\{7 e=\$\(4\).X\(\);\$.1N\(\{1c:"/1O/1d/J/-/"\+e\+"\?1e=1P-1Q-1R&1S-1T=3",1x:"1U",1V:"1W",1X:6\(e\)\{1f\(7 t="",a=\'<b m="L-2T">\',r=0;r<e.8.9.1g;r\+\+\)\{1f\(7 n=0;n<e.8.9\[r\].Q.1g;n\+\+\)C\("1Z"==e.8.9\[r\].Q\[n\].20\)\{t=e.8.9\[r\].Q\[n\].z;21\}7 i=e.8.9\[r\].j.\$t,l=\(e.8.9\[r\].22\[0\].23,e.8.9\[r\].2U\[0\].2V.\$t,e.8.9\[r\].I.\$t\),s=\$\("<b>"\).1h\(l\);C\(l.1i\("//25.26.1y/27/"\)>-1\)7 d=e.8.9\[r\].28\$29.1c.R\("/J.S","/1z.S"\),c=d;1j C\(l.1i\("<v"\)>-1\)7 f=s.k\("v:2a"\).g\("1k"\).R\("1A-c","1B"\),c=f;1j 7 c=1s;a\+=\'<1l><b m="L-v"><a z="\'\+t\+\'"><v 1e="\'\+i\+\'" 1k="\'\+c\+\'" j="\'\+i\+\'"/></a></b><b m="L-I"><1m m="L-j"><a z="\'\+t\+\'" j="\'\+i\+\'">\'\+i\+"</a></1m></b></1l>"\}a\+=\'</b><b m="2c"/>\',\$\("#L-1d"\).1h\(a\),\$\(4\).k\(".L-v"\).B\(6\(\)\{\$\(4\).g\("1n",6\(e,t\)\{K t.R\("/J.S","/1z.S"\)\}\).g\("1n",6\(e,t\)\{K t.R\("1A-c","1B"\)\}\)\}\)\}\}\)\}\);6 2d\(e\)\{7 t=!0;C\(5\("#"\+e\+" .1o"\).B\(6\(\)\{""!=\$.1p\(5\(4\).T\(\)\)&&"J"!=\$.1p\(5\(4\).T\(\)\)\|\|5\(4\).q\("Y"\)\}\),5\("#"\+e\+" .1o"\).B\(6\(\)\{K""==\$.1p\(5\(4\).T\(\)\)\?\(t=!1,5\(4\).M\("x"\).k\(".Z"\).q\("N"\),5\(4\).Y\(\),!1\):"J"==\$.1p\(5\(4\).T\(\)\)\?\(t=!1,5\(4\).M\("x"\).k\(".Z"\).q\("N"\),!1\):2W 0\}\),t===!0\)\{7 i="",a="1G://2X.1q.1y/2e";C\(/2Y\|2Z\|30\|31\|32\|33\|34\|35 36/i.37\(38.39\)&&\(a="1q://2e"\),"3a"===e\)7 s=3b,r=3c,n="3d",o="3e",l=\(5\("#"\+e\+" .j-I"\).X\(\),5\("#"\+e\+" .3f"\).T\(\)\),i=a\+"\?3g="\+s\+r\+"&X=\*"\+o\+" "\+n\+"...\* %2f%2f"\+l;5\(4\).g\("z",i\);7 u=3h,h=3i,c=2g\(2h.2i/2-u/2\),d=2g\(2h.2j/2-h/2\),y=1t.1F\(4.z,"","3j=w, 3k=w, 3l=w, 3m=w, 3n=w, 3o=3p, 3q=1, 3r=w, 2i="\+u\+", 2j="\+h\+", 3s="\+d\+", 3t="\+c\);K y.Y\(\),!1\}\}1t.3u\("3v",6\(\)\{7 e=5\("j"\).X\(\);5\(".3w"\).D\("E",6\(\)\{5\(4\).p\("N"\),5\("j"\).X\(e\)\}\),5\(".O 3x, .O 3y"\).D\("3z",6\(\)\{13===3A.3B&&5\(4\).M\(".O"\).k\(".2k"\).3C\("E"\)\}\),5\(".O .1o"\).B\(6\(\)\{j=5\(4\).g\("3D"\),x=5\(4\).M\("x"\),5\(\'<W m="Z">\(3E\)</W>\'\).3F\(x\)\}\),5\(".O .1o"\).3G\(6\(\)\{""!=5\(4\).T\(\)&&\(5\(4\).p\("Y"\),5\(4\).M\("x"\).k\(".Z"\).p\("N"\)\)\}\),5\(".O 3H"\).3I\(6\(\)\{5\(4\).p\("Y"\),5\(4\).M\("x"\).k\(".Z"\).p\("N"\)\}\)\},!1\),5\(".O .2k"\).D\("E",6\(\)\{K 2d\(5\(4\).M\(".3J"\).g\("1w"\)\),!1\}\),\$\(A\).D\("E",".1a-1C",6\(\)\{\$\("#1q-1C"\).q\("2l"\).p\("N"\)\}\),\$\(A\).D\("E",".3K-3L",6\(\)\{\$\("#1q-1C"\).q\("N"\).p\("2l"\)\}\);', 62, 234, "\|\|\|\|this\|jQuery\|function\|var\|feed\|entry\|\|div\|\|\|\|\|attr\|\|\|title\|find\|\|class\|\|\|removeClass\|addClass\|\|\|\|\|img\|no\|label\|\|href\|document\|each\|if\|on\|click\|header\|fixed\|recent\|content\|default\|return\|related\|parents\|show\|formWA\|sidebar\|link\|replace\|jpg\|val\|AM\|scroll\|span\|text\|focus\|validasi\|\|console\|\|\|log\|The\|time\|is\|PM\|menu\|close\|scrollTop\|url\|posts\|alt\|for\|length\|html\|indexOf\|else\|src\|li\|h3\|style\|wajib\|trim\|whatsapp\|clock\|no_image\|window\|data\|parent\|id\|type\|com\|mqdefault\|s72\|s1600\|chat\|getElementById\|innerHTML\|open\|https\|css\|position\|sticky\|goom\|HTML\|widget\|ajax\|feeds\|json\|in\|script\|max\|results\|get\|dataType\|jsonp\|success\|ul\|alternate\|rel\|break\|category\|term\|\|www\|youtube\|embed\|media\|thumbnail\|first\|rmg\|clr\|kirimWA\|send\|0A\|Number\|screen\|width\|height\|submit\|hide\|new\|Date\|getMinutes\|getSeconds\|getHours\|hours\|minutes\|setInterval\|1e3\|goomsite\|github\|io\|non\|webp\|month_format\|Jan\|Feb\|Mar\|Apr\|May\|Jun\|Jul\|Aug\|Sept\|Oct\|Nov\|Dec\|outerHeight\|50\|match\|tag\|icon\|search\|wrap\|author\|name\|void\|web\|Android\|webOS\|iPhone\|iPad\|iPod\|BlackBerry\|IEMobile\|Opera\|Mini\|test\|navigator\|userAgent\|konsultasi\|62\|83138538450\|Admin\|Hello\|pesan\|phone\|960\|540\|toolbar\|location\|directories\|status\|menubar\|scrollbars\|yes\|resizable\|copyhistory\|top\|left\|addEventListener\|load\|waFix\|input\|textarea\|keypress\|event\|keyCode\|trigger\|placeholder\|Required\|appendTo\|keyup\|select\|change\|poptamv\|nabil\|live".split("\|"), 0, {}))
+var lazyadsense = false;
+window.addEventListener("scroll", function () {
+    if ((document.documentElement.scrollTop != 0 && lazyadsense === false) || (document.body.scrollTop != 0 && lazyadsense === false)) {
+        (function () {
+            var ad = document.createElement('script');
+            ad.type = 'text/javascript';
+            ad.async = true;
+            ad.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+            var sc = document.getElementsByTagName('script')[0];
+            sc.parentNode.insertBefore(ad, sc);
+        })();
+        lazyadsense = true;
+    }
+}, true);
+(function (a) {
+    a.fn.lazyload = function (b) {
+        var c = {
+            threshold: 0,
+            failurelimit: 0,
+            event: "scroll",
+            effect: "show",
+            kontoltener: window
+        };
+        if (b) {
+            a.extend(c, b)
+        }
+        var d = this;
+        if ("scroll" == c.event) {
+            a(c.kontoltener).bind("scroll", function (b) {
+                var e = 0;
+                d.each(function () {
+                    if (a.abovethetop(this, c) || a.leftofbegin(this, c)) {} else if (!a.belowthefold(this, c) && !a.rightoffold(this, c)) {
+                        a(this).trigger("appear")
+                    } else {
+                        if (e++ > c.failurelimit) {
+                            return false
+                        }
+                    }
+                });
+                var f = a.grep(d, function (a) {
+                    return !a.loaded
+                });
+                d = a(f)
+            })
+        }
+        this.each(function () {
+            var b = this;
+            if (undefined == a(b).attr("original")) {
+                a(b).attr("original", a(b).attr("src"))
+            }
+            if ("scroll" != c.event || undefined == a(b).attr("src") || c.placeholder == a(b).attr("src") || a.abovethetop(b, c) || a.leftofbegin(b, c) || a.belowthefold(b, c) || a.rightoffold(b, c)) {
+                if (c.placeholder) {
+                    a(b).attr("src", c.placeholder)
+                } else {
+                    a(b).removeAttr("src")
+                }
+                b.loaded = false
+            } else {
+                b.loaded = true
+            }
+            a(b).one("appear", function () {
+                if (!this.loaded) {
+                    a("<img/>").bind("load", function () {
+                        a(b).hide().attr("src", a(b).attr("original"))[c.effect](c.effectspeed);
+                        b.loaded = true
+                    }).attr("src", a(b).attr("original"))
+                }
+            });
+            if ("scroll" != c.event) {
+                a(b).bind(c.event, function (c) {
+                    if (!b.loaded) {
+                        a(b).trigger("appear")
+                    }
+                })
+            }
+        });
+        a(c.kontoltener).trigger(c.event);
+        return this
+    };
+    a.belowthefold = function (b, c) {
+        if (c.kontoltener === undefined || c.kontoltener === window) {
+            var d = a(window).height() + a(window).scrollTop()
+        } else {
+            var d = a(c.kontoltener).offset().top + a(c.kontoltener).height()
+        }
+        return d <= a(b).offset().top - c.threshold
+    };
+    a.rightoffold = function (b, c) {
+        if (c.kontoltener === undefined || c.kontoltener === window) {
+            var d = a(window).width() + a(window).scrollLeft()
+        } else {
+            var d = a(c.kontoltener).offset().left + a(c.kontoltener).width()
+        }
+        return d <= a(b).offset().left - c.threshold
+    };
+    a.abovethetop = function (b, c) {
+        if (c.kontoltener === undefined || c.kontoltener === window) {
+            var d = a(window).scrollTop()
+        } else {
+            var d = a(c.kontoltener).offset().top
+        }
+        return d >= a(b).offset().top + c.threshold + a(b).height()
+    };
+    a.leftofbegin = function (b, c) {
+        if (c.kontoltener === undefined || c.kontoltener === window) {
+            var d = a(window).scrollLeft()
+        } else {
+            var d = a(c.kontoltener).offset().left
+        }
+        return d >= a(b).offset().left + c.threshold + a(b).width()
+    };
+    a.extend(a.expr[":"], {
+        "below-the-fold": "$.belowthefold(a, {threshold : 0, kontoltener: window})",
+        "above-the-fold": "!$.belowthefold(a, {threshold : 0, kontoltener: window})",
+        "right-of-fold": "$.rightoffold(a, {threshold : 0, kontoltener: window})",
+        "left-of-fold": "!$.rightoffold(a, {threshold : 0, kontoltener: window})"
+    })
+})(jQuery);
+$(function () {
+    $("img").lazyload({
+        placeholder: "https://1.bp.blogspot.com/-Q4QqdyWtLSI/XghECAoZUoI/AAAAAAAABs0/NFJsaHuy-PU6HIeIf960bwvdpEAXEJTZACLcBGAsYHQ/s1600/goom.gif",
+        effect: "fadeIn",
+        threshold: "-50"
+    })
+});
